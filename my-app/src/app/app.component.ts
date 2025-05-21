13743915da13787fa4caa4b7d06a264c5bf07cad
CyberMcConaughey <154449376+CyberMcConaughey@users.noreply.github.com>
2025-05-21 15:24:57 +0300

    замена кода

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
-import { UsrBPMCheckList } from "./checklist/checklist.component";
+import { UsrAngularCheck } from "./checklist/checklist.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [UsrBPMCheckList],
+  imports: [UsrAngularCheck],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
